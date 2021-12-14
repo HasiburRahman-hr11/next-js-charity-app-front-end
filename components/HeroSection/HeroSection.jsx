71e4ccd0972a82cfded39a13590f33512f41734e
hrhasib <mailtohrhasib@gmail.com>
2021-12-14 21:32:59 +0600

    added hero section

--- components/HeroSection/HeroSection.jsx
+++ components/HeroSection/HeroSection.jsx
@@ -4,70 +4,86 @@ import Image from 'next/image';
 
 import { Container, Grid, Box, Typography } from '@mui/material';
 
-import styles from './HeroSection.module.css';
+const styles = {
+    container: {
+        position: 'relative',
+        minHeight: {
+            lg: '950px'
+        },
+        padding: '100px 0'
+    },
+    heroRight: {
+        position: 'absolute',
+        display: {
+            lg: 'block',
+            xs: 'none'
+        },
+        right: '0',
+        top: '0',
+        width: '800px',
+        height: '727px',
+        zIndex: '-1',
+        '&::before': {
+            content: '',
+            content: '""',
+            background: `url('/images/hero.png') 50%/cover no-repeat local`,
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            right: '-100px',
+            bottom: '0',
+            top: '100px'
+        }
+    }
+}
+
 const HeroSection = () => {
     return (
         <Box component="section" className='section hero__section' sx={{
-            overflow:'hidden'
+            overflow: 'hidden'
         }}>
-            <Container fixed sx={{
-                position: 'relative',
-                minHeight:{
-                    md:'950px'
-                },
-                paddingTop:'100px'
-            }}>
-                <Grid container sx={{
-                height:'100%'
-            }}>
-                    <Grid item xs={12} md={4}>
-                        <Box component="div">
-                            <Typography variant='h1' component="h1" >
+            <Container fixed sx={styles.container}>
+                <Grid container>
+                    <Grid item xs={12} lg={5}>
+                        <Box component="div" sx={{
+                            paddingTop: { lg: '130px' }
+                        }}>
+                            <Typography variant='h1' component="h1" sx={{
+                                fontSize: '70px',
+                                fontFamily: "'Oswald', sans-serif",
+                                fontWeight:'700',
+                                color:'var(--title-color)',
+                                marginBottom:'35px'
+                            }}>
                                 Lets be kind for
-                                <Box component="span">Children</Box>
+                                <Box component="span" sx={{
+                                    color: 'var(--primary-color)',
+                                    display: {
+                                        lg: 'block',
+                                        xs: 'inline-block'
+                                    }
+                                }}>Children</Box>
                             </Typography>
-                            <Typography variant='p' component="p">
+                            <Typography variant='p' component="p" sx={{
+                                color:'#666',
+                                fontSize:'20px',
+                                lineHeight:'27px',
+                                maxWidth:'450px',
+                                marginBottom:'35px'
+                            }}>
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate perferendis sapiente aut.
                             </Typography>
                             <Link href="/cases">
-                                <a>Donate Now</a>
+                                <a className='btn__primary'>Donate Now</a>
                             </Link>
                         </Box>
                     </Grid>
-                    {/* <Grid item xs={12} md={6}>
-                        <Box component="div" sx={{
-                            position: 'relative'
-
-                        }}>
-                            <Image
-                                src='/images/hero.png'
-                                alt="Hero Banner"
-                            />
-                        </Box>
-                    </Grid> */}
                 </Grid>
-                <Box className="hero__right" sx={{
-                    position: 'absolute',
-                    right: '0',
-                    top: '0',
-                    width: '800px',
-                    height: '727px',
-                    zIndex: '-1',
-                    '&::before': {
-                        content: '',
-                        content: '""',
-                        background: `url('/images/hero.png') 50%/cover no-repeat local`,
-                        width: '100%',
-                        height: '100%',
-                        position: 'absolute',
-                        right: '-100px',
-                        bottom: '0',
-                        top: '100px'
-                    }
-                }}></Box>
+                <Box className="hero__right" sx={styles.heroRight}></Box>
             </Container>
         </Box>
     );
 };
 
 export default HeroSection;
+
