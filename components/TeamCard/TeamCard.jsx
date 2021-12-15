109a1bcdfe29396185a9047ee02e6f2c56a0f40f
hrhasib <mailtohrhasib@gmail.com>
2021-12-15 15:53:08 +0600

    added team section

---
+++ components/TeamCard/TeamCard.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+
+
+const styles = {
+    info: {
+        textAlign: 'center',
+        backgroundColor: '#fff',
+        width: 'calc(100% - 50px)',
+        height: '110px',
+        overflow: 'hidden',
+        position: 'absolute',
+        right: '0',
+        bottom: '-50px',
+        padding: '30px',
+        boxShadow: 'rgba(0, 0, 0, 0.1) 0px 4px 6px -1px, rgba(0, 0, 0, 0.06) 0px 2px 4px -1px',
+        transition: '0.4s all',
+        '& .team__title': {
+            fontFamily: "'Oswald', sans-serif",
+            fontSize: '23px',
+            fontWeight: '600',
+            color: 'var(--title-color)',
+            marginBottom: '10px'
+        },
+        '& .team__sub': {
+            color: '#666',
+            marginBottom: '25px',
+            fontSize: '18px'
+        },
+        '& .team__icon': {
+            color: 'var(--title-color)',
+            fontSize: '18px'
+        },
+        '&:hover': {
+            height: '160px',
+            backgroundColor: 'var(--primary-color)'
+        },
+        '&:hover .team__title , &:hover .team__sub,&:hover .team__icon': {
+            color: '#fff'
+        }
+    }
+}
+
+const TeamCard = ({ team }) => {
+    return (
+        <Box component="div" className='team__item' sx={{
+            position: 'relative'
+        }}>
+            <Box component="div" className="team__image">
+                <img src={team.image} alt={team.name} />
+            </Box>
+            <Box component="div" className="team__info" sx={styles.info}>
+                <Typography variant="h4" component="h4" className='team__title'>
+                    {team.name}
+                </Typography>
+                <Typography variant="p" component="p" className='team__sub'>
+                    {team.designation}
+                </Typography>
+                <Box component="ul" className="team__social" sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    {team.social.map((item, index) => (
+                        <li key={index} style={{ margin: '0 10px' }}>
+                            <a href={item.link} target="_blank" className='team__icon'>
+                                {item.icon}
+                            </a>
+                        </li>
+                    ))}
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default TeamCard;
--- components/TeamSection/TeamSection.jsx
+++ components/TeamSection/TeamSection.jsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import { Container, Grid, Box } from '@mui/material';
+import SectionHeader from '../SectionHeader/SectionHeader';
+import { FaFacebookF, FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa'
+import TeamCard from '../TeamCard/TeamCard';
+
+const teamData = [
+    {
+        name: 'Nick Fury',
+        designation: 'CEO & Founder',
+        image:'/images/team-1.jpg',
+        social: [
+            { icon: <FaFacebookF />, link: '/' },
+            { icon: <FaLinkedinIn />, link: '/' },
+            { icon: <FaGithub />, link: '/' },
+            { icon: <FaTwitter />, link: '/' }
+        ]
+    },
+    {
+        name: 'Scott Smith',
+        designation: 'Volunteer',
+        image:'/images/team-2.jpg',
+        social: [
+            { icon: <FaFacebookF />, link: '/' },
+            { icon: <FaLinkedinIn />, link: '/' },
+            { icon: <FaGithub />, link: '/' },
+            { icon: <FaTwitter />, link: '/' }
+        ]
+    },
+    {
+        name: 'Annie Antonio',
+        designation: 'Volunteer',
+        image:'/images/team-3.jpg',
+        social: [
+            { icon: <FaFacebookF />, link: '/' },
+            { icon: <FaLinkedinIn />, link: '/' },
+            { icon: <FaGithub />, link: '/' },
+            { icon: <FaTwitter />, link: '/' }
+        ]
+    }
+]
 
 const TeamSection = () => {
     return (
-        <section className='section team__section'>
-            <h2>Team Section</h2>
-        </section>
+        <Box component="section" className='section team__section' sx={{
+            padding: '100px 0'
+        }}>
+            <Container fixed>
+                <SectionHeader
+                    title="Our Expert Volunteer"
+                    subtitle="Meet Our Team"
+                />
+                <Box component="div" className='section__body' sx={{
+                    marginTop: '70px'
+                }}>
+                    <Grid container spacing={{ xs: 5, sm: 4 }}>
+                        {teamData.map((team, index) => (
+                            <Grid item xs={12} sm={6} md={4} key={index} sx={{
+                                marginBottom:{
+                                    xs:'70px',
+                                    md:'0'
+                                }
+                            }}>
+                                <TeamCard team={team} />
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Box>
+            </Container>
+        </Box>
     );
 };
 
