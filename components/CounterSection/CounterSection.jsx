c18158233ffef1829c0924f8e4099aa8f1853559
hrhasib <mailtohrhasib@gmail.com>
2021-12-15 15:10:51 +0600

    added counter section

--- components/CounterSection/CounterSection.jsx
+++ components/CounterSection/CounterSection.jsx
@@ -1,10 +1,89 @@
 import React from 'react';
+import { Box, Container, Grid, Typography } from '@mui/material';
+
+
+const counters = [
+    {
+        title: '3500',
+        text: 'Donations'
+    },
+    {
+        title: '50',
+        text: 'Fund Raised'
+    },
+    {
+        title: '125',
+        text: 'Volunteers'
+    },
+    {
+        title: '340',
+        text: 'Projects'
+    }
+];
+
+const styles = {
+    section: {
+        background: "linear-gradient(rgba(6,34,101,.55),rgba(6,34,101,.55)), url(/images/counter-bg.jpg)",
+        padding: '100px 0',
+        textAlign: 'center'
+    },
+    title: {
+        fontFamily: "'Oswald', sans-serif",
+        fontSize: {
+            sm:'50px',
+            xs:'35px'
+        },
+        marginBottom: '5px',
+        fontWeight: '600',
+        color: '#fff'
+    },
+    text: {
+        color: '#999',
+        fontSize: {
+            sm:'20px',
+            xs:'18px'
+        }
+    }
+}
 
 const CounterSection = () => {
     return (
-        <section className='section counter__section'>
-            <h2>Counter Section</h2>
-        </section>
+        <Box component="section" className='section counter__section' sx={styles.section}>
+            <Container fixed>
+                <Grid container spacing={5}>
+                    {counters.map((item, index) => (
+                        <Grid item xs={6} md={3} key={index} sx={{
+                            position: 'relative',
+                            '&::before': {
+                                content: '""',
+                                position: 'absolute',
+                                top: '57px',
+                                right: '0',
+                                width: '1px',
+                                height: '70px',
+                                background: '#636363',
+                                display:{
+                                    xs:'none',
+                                    md:'block'
+                                }
+                            },
+                            ":last-child::before": {
+                                display: "none"
+                            }
+                        }}>
+                            <Box component="div">
+                                <Typography variant="h2" component="h2" sx={styles.title}>
+                                    {item.title}+
+                                </Typography>
+                                <Typography variant="p" component="p" sx={styles.text}>
+                                    {item.text}
+                                </Typography>
+                            </Box>
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+        </Box>
     );
 };
 
