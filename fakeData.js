0f63696a68cc30151daba7b43595119045c5d33b
hrhasib <mailtohrhasib@gmail.com>
2021-12-18 20:39:37 +0600

    final touch

--- fakeData.js
+++ fakeData.js
@@ -1,44 +1,6 @@
 
 import { FaFacebookF, FaLinkedinIn, FaGithub, FaTwitter } from 'react-icons/fa';
 
-export const cases = [
-    {
-        _id: '01',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '02',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '03',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '04',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '05',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '06',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    },
-    {
-        _id: '07',
-        title: 'Ensure Education for every poor children',
-        thumbnail: 'https://images.pexels.com/photos/8618062/pexels-photo-8618062.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260'
-    }
-];
-
 
 export const teamData = [
     {
@@ -77,29 +39,3 @@ export const teamData = [
 ];
 
 
-export const blogs = [
-    {
-        _id: '01',
-        title: 'Many Children are suffering a lot for food.',
-        description: 'Some description text',
-        thumbnail: 'https://images.pexels.com/photos/8078392/pexels-photo-8078392.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-        author: 'Mr. Invisible',
-        createdAt: '2021-12-15T14:05:42.138Z'
-    },
-    {
-        _id: '02',
-        title: 'Be soft and kind for the poor people.',
-        description: 'Some description text',
-        thumbnail: 'https://images.pexels.com/photos/7293100/pexels-photo-7293100.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-        author: 'Mr. Invisible',
-        createdAt: '2021-12-15T14:05:42.138Z'
-    },
-    {
-        _id: '03',
-        title: 'Be kind for the poor people and the kids.',
-        description: 'Some description text',
-        thumbnail: 'https://images.pexels.com/photos/1125850/pexels-photo-1125850.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
-        author: 'Mr. Invisible',
-        createdAt: '2021-12-15T14:05:42.138Z'
-    }
-];
